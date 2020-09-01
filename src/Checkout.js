1e2c5da7a21a338c93f918fb180ea7d1a4b23071
Mariana <marianatorrensf@gmail.com>
2020-09-01 17:25:26 -0400

    checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {useStateValue} from "./StateProvider";
+import './Checkout.css';
 
 function Checkout() {
     const [{basket}] = useStateValue();
 
     return <div className="checkout">
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad"/>
+
 
     </div>;
 }
